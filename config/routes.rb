c4b18d040e8c0b9b58ed25440d2f2ea789f40bdd
DarkyRobin <velascoayen@gmail.com>
2023-11-23 04:14:46 +1100

    No done commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :posts
   resources :articles
+  # resources :users
+
+  # root "users#index"
+  get '/users' => 'users#index'
+  get '/users/new' => 'users#new', as: 'new_user'
+  post '/users' => 'users#create', as: 'create_user'
+  delete '/users/:id', to: 'users#destroy', as: 'delete_user'
+  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
+  patch '/users/:id' => 'users#update', as: 'update_user'
+  get '/users' => 'users#show', as: 'user'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
